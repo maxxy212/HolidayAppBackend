64c986d2263897ee7d8cfbe2fe8a6d3edc05f27f
lidichamp <lidichamp@gmail.com>
2021-04-22 02:57:37 +0100

    getallusers

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,12 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-Route::get('holidays', 'Api\HolidayController@getAllHolidays');
-Route::get('holidays/{id}', 'Api\HolidayController@getHolidayById');
-Route::get('holidays/{start_date}/{end_date}', 'Api\HolidayController@getHolidayByDate');
-Route::post('holidays', 'Api\HolidayController@createHoliday');
-Route::delete('holidays/{id}','Api\HolidayController@deleteHoliday');
+Route::get('holidays', 'App\Http\Controllers\Api\HolidayController@getAllHolidays');
+Route::get('holidays/{id}', 'App\Http\Controllers\Api\HolidayController@getHolidayById');
+Route::get('holidays/{start_date}/{end_date}', 'App\Http\Controllers\Api\HolidayController@getHolidayByDate');
+Route::post('holidays', 'App\Http\Controllers\Api\HolidayController@createHoliday');
+Route::delete('holidays/{id}','App\Http\Controllers\Api\HolidayController@deleteHoliday');
   });
-  Route::get('all/users', 'Api\AuthController@all_user');
-Route::post('/login', 'Api\AuthController@login');
-Route::post('/logout', 'Api\AuthController@logout');
+  Route::get('all/users', 'App\Http\Controllers\Api\HolidayController@all_user');
+Route::post('/login', 'App\Http\Controllers\Api\HolidayController@login');
+Route::post('/logout', 'App\Http\Controllers\Api\HolidayController@logout');
